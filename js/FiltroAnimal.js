5b8e39c124961823851a580e22998296f55489c7
EnyiZH <enyizhou@gmail.com>
2024-03-06 10:04:59 +0000

    Cambiado el JavaScript del FiltroAnimal y completado la funcionalidad del filtracion

--- js/FiltroAnimal.js
+++ js/FiltroAnimal.js
@@ -1,31 +1,10 @@
 
-  document.addEventListener("DOMContentLoaded", function () {
-    // Obtener los botones de filtro
-    var filterButtons = document.querySelectorAll(".filtro-catalogo");
-
-    // Agregar un event listener a cada botón de filtro
-    filterButtons.forEach(function (button) {
-      button.addEventListener("click", function () {
-        // Remover la clase 'active-btn' de todos los botones
-        filterButtons.forEach(function (btn) {
-          btn.classList.remove("active-btn");
-        });
-
-        // Agregar la clase 'active-btn' al botón clickeado
-        button.classList.add("active-btn");
-
-        // Filtrar animales según la categoría del botón clickeado
-        var category = button.id;
-        filterAnimals(category);
-      });
-    });
-
-    // Función para filtrar animales según la categoría
+  (function () {
     function filterAnimals(category) {
-      var animals = document.querySelectorAll(".Animal");
+      const animals = document.querySelectorAll(".Animal");
 
       animals.forEach(function (animal) {
-        var animalCategories = animal.classList;
+        const animalCategories = animal.classList;
         if (category === "All" || animalCategories.contains(category)) {
           // Mostrar el animal si la categoría es "All" o coincide con la categoría del botón
           animal.style.display = "block";
@@ -35,4 +14,34 @@
         }
       });
     }
-  });
+
+    function addListeners() {
+      for (const filtro of document.querySelectorAll(".filtro-catalogo")) {
+        filtro.addEventListener("click", (ev) => filtrar(filtro.id, ev));
+      }
+    }
+
+    function filtrar(category, event) {
+      event.preventDefault();
+
+      // Obtener todos los botones de filtro
+      const filterButtons = document.querySelectorAll(".filtro-catalogo");
+
+      // Remover la clase 'active-btn' de todos los botones
+      filterButtons.forEach(function (btn) {
+        btn.classList.remove("active-btn");
+      });
+
+      // Agregar la clase 'active-btn' al botón clickeado
+      event.target.classList.add("active-btn");
+
+      // Filtrar animales según la categoría del botón clickeado
+      filterAnimals(category);
+    }
+
+    if (document.readyState === 'loading') {  // Si el documento aún se está cargando
+      document.addEventListener('DOMContentLoaded', addListeners);
+    } else {  // Si el documento ya está cargado
+      addListeners();
+    }
+  })();

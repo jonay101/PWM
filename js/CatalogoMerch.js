3960b566398ab3631073036be4c1e09e07474904
epictuno <tanayjonay2@gmail.com>
2024-03-07 18:56:05 +0000

    Cambios a tags para que se utilicen los de html5 (section,ul,li...)

--- js/CatalogoMerch.js
+++ js/CatalogoMerch.js
@@ -3,7 +3,7 @@
   let productos = ["Producto 1", "Producto 2", "Producto 3", "Producto 4", "Producto 5"];
   let precios = ["15 €", "25 €", "35 €", "45 €", "55 €"];
 
-  let catalogoMerchandising = document.querySelector("main ul")
+  let catalogoMerchandising = document.querySelector("main ul");
   for (i = 0; i < productos.length; i++) {
     let nuevoElemento = template.cloneNode(true);
     nuevoElemento.querySelector('.info').textContent = productos[i];

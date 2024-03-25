d89dc6c87428154fa515aece16cf3cdd962aed54
Agus <aagiribar@yahoo.es>
2024-03-25 21:02:21 +0000

    Refactorizado código de Carrito.js y pequeño arreglo realizado en la página de legal

--- js/Carrito.js
+++ js/Carrito.js
@@ -7,31 +7,8 @@
   let mainElement = document.querySelector("main");
   mainElement.appendChild(template.querySelector("link"));
 
-  let entradas = datos["Entradas"];
-  let fragmentoEntradas = document.createDocumentFragment();
-  let ulDivEntradas = document.querySelector("section#lista-entradas div ul");
-  for (var i = 0; i < entradas.length; i++) {
-    let nuevoElemento = elementoLista.cloneNode(true);
-    nuevoElemento.querySelector('.informacion').textContent = entradas[i]["nombre"];
-    nuevoElemento.querySelector('.precio').textContent = `${entradas[i]["coste"]} €`;
-    nuevoElemento.querySelector('.cantidad').textContent = entradas[i]["cantidad"];
-    nuevoElemento.querySelector('.imagen').src = entradas[i]["imagen"];
-    fragmentoEntradas.appendChild(nuevoElemento);
-  }
-  ulDivEntradas.appendChild(fragmentoEntradas);
-
-  let productos = datos["Merchandising"];
-  let fragmentoMerchandising = document.createDocumentFragment();
-  let ulDivMerchandising = document.querySelector("section#lista-merchandising div ul");
-  for (i = 0; i < productos.length; i++) {
-    let nuevoElemento = elementoLista.cloneNode(true);
-    nuevoElemento.querySelector('.informacion').textContent = productos[i]["nombre"];
-    nuevoElemento.querySelector('.precio').textContent = `${productos[i]["coste"]} €`;
-    nuevoElemento.querySelector('.cantidad').textContent = productos[i]["cantidad"];
-    nuevoElemento.querySelector('.imagen').src = productos[i]["imagen"];
-    fragmentoMerchandising.appendChild(nuevoElemento);
-  }
-  ulDivMerchandising.appendChild(fragmentoMerchandising);
+  insertarFragmento("entradas", datos, elementoLista);
+  insertarFragmento("merchandising", datos, elementoLista);
 
   cargarListenersEliminar();
 })();
@@ -44,4 +21,19 @@ function cargarListenersEliminar() {
 function eliminarElemento(evt) {
   let elementoCarrito = evt.target.parentNode.parentNode.parentNode;
   elementoCarrito.parentNode.removeChild(elementoCarrito);
+}
+
+function insertarFragmento(listado, datos, elementoLista) {
+  let elementos = datos[listado];
+  let fragmento = document.createDocumentFragment();
+  let ulDivListado = document.querySelector(`section#lista-${listado} div ul`);
+  for (var i = 0; i < elementos.length; i++) {
+    let nuevoElemento = elementoLista.cloneNode(true);
+    nuevoElemento.querySelector('.informacion').textContent = elementos[i]["nombre"];
+    nuevoElemento.querySelector('.precio').textContent = `${elementos[i]["coste"]} €`;
+    nuevoElemento.querySelector('.cantidad').textContent = elementos[i]["cantidad"];
+    nuevoElemento.querySelector('.imagen').src = elementos[i]["imagen"];
+    fragmento.appendChild(nuevoElemento);
+  }
+  ulDivListado.appendChild(fragmento);
 }

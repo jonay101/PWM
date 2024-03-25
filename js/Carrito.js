6c83ab1ad15b9491b2b6eaeb7e511640af69fb0a
Agus <aagiribar@yahoo.es>
2024-03-25 20:51:29 +0000

    Implementada carga dinámica de contenido del carrito a través de JSON

--- js/Carrito.js
+++ js/Carrito.js
@@ -1,29 +1,34 @@
 (async function() {
   let template = (await window.cargarSubTemplate("../html/ListadoCarrito.html"));
   let elementoLista = template.querySelector("ul li");
-  let entradas = ["Entrada 1", "Entrada 2", "Entrada 3", "Entrada x"];
-  let productos = ["Producto 1", "Producto 2", "Producto 3", "Producto 4"];
-  let precios = ["15 €", "25 €", "35 €", "45 €"];
+
+  let datos = await window.cargarJSON("Carrito.json");
 
   let mainElement = document.querySelector("main");
   mainElement.appendChild(template.querySelector("link"));
 
+  let entradas = datos["Entradas"];
   let fragmentoEntradas = document.createDocumentFragment();
   let ulDivEntradas = document.querySelector("section#lista-entradas div ul");
   for (var i = 0; i < entradas.length; i++) {
     let nuevoElemento = elementoLista.cloneNode(true);
-    nuevoElemento.querySelector('.informacion').textContent = entradas[i];
-    nuevoElemento.querySelector('.precio').textContent = precios[i];
+    nuevoElemento.querySelector('.informacion').textContent = entradas[i]["nombre"];
+    nuevoElemento.querySelector('.precio').textContent = `${entradas[i]["coste"]} €`;
+    nuevoElemento.querySelector('.cantidad').textContent = entradas[i]["cantidad"];
+    nuevoElemento.querySelector('.imagen').src = entradas[i]["imagen"];
     fragmentoEntradas.appendChild(nuevoElemento);
   }
   ulDivEntradas.appendChild(fragmentoEntradas);
 
+  let productos = datos["Merchandising"];
   let fragmentoMerchandising = document.createDocumentFragment();
   let ulDivMerchandising = document.querySelector("section#lista-merchandising div ul");
   for (i = 0; i < productos.length; i++) {
     let nuevoElemento = elementoLista.cloneNode(true);
-    nuevoElemento.querySelector('.informacion').textContent = productos[i];
-    nuevoElemento.querySelector('.precio').textContent = precios[i];
+    nuevoElemento.querySelector('.informacion').textContent = productos[i]["nombre"];
+    nuevoElemento.querySelector('.precio').textContent = `${productos[i]["coste"]} €`;
+    nuevoElemento.querySelector('.cantidad').textContent = productos[i]["cantidad"];
+    nuevoElemento.querySelector('.imagen').src = productos[i]["imagen"];
     fragmentoMerchandising.appendChild(nuevoElemento);
   }
   ulDivMerchandising.appendChild(fragmentoMerchandising);

832678efd26f820888227b9ee5e102480e0d7a71
Agus <aagiribar@yahoo.es>
2024-03-26 23:22:07 +0000

    Implementada funcionalidad de añadir entradas al carrito al pulsar el botón de comprar

--- js/Entradas.js
+++ js/Entradas.js
@@ -1,8 +1,10 @@
 (async function() {
-  agregarListeners();
+  let datos = await window.cargarJSON("Entradas.json");
+  let listaEntradas = datos["Entradas"]
+  agregarListeners(listaEntradas);
 })();
 
-function agregarListeners() {
+function agregarListeners(listaEntradas) {
   for (const botonMenos of document.querySelectorAll("main section#entradas button.boton-menos")) {
     botonMenos.addEventListener('click', decrementarValor);
   }
@@ -10,6 +12,9 @@ function agregarListeners() {
   for (const botonMas of document.querySelectorAll("main section#entradas button.boton-mas")) {
     botonMas.addEventListener('click', incrementarValor);
   }
+
+  let botonComprar = document.querySelector("main button#boton-comprar");
+  botonComprar.addEventListener('click', (evt) => agregarAlCarrito(evt, listaEntradas));
 }
 
 function decrementarValor(evt) {
@@ -36,4 +41,29 @@ function incrementarValor(evt) {
   else {
     inputElement.value = (valor + 1).toString();
   }
+}
+
+async function agregarAlCarrito(evt, listaEntradas) {
+  let carrito = JSON.parse(window.localStorage.getItem("carrito"));
+  for (const divEntrada of document.querySelectorAll("main section#entradas div#formulario-entradas div")) {
+    let cantidad = parseInt(divEntrada.querySelector("input").value);
+    let etiqueta = divEntrada.querySelector("label").textContent.trim();
+    let entrada = listaEntradas.find(p => p.nombre === etiqueta.trim().substring(0, etiqueta.length - 1));
+    if (cantidad > 0 && !isNaN(cantidad) && entrada) {
+      let nombre = entrada.nombre;
+      let coste = entrada.coste;
+      let entradaCarrito = {
+        nombre: nombre,
+        coste: coste,
+        cantidad: cantidad,
+        imagen: "icono_entrada.png"
+      }
+      carrito["entradas"].push(entradaCarrito);
+      console.log(`Añadido ${entrada.nombre} | ${cantidad} al carrito`);
+    }
+    else {
+      console.log(`No se ha podido añadir ${entrada.nombre} | ${cantidad} al carrito`);
+    }
+  }
+  window.localStorage.setItem("carrito", JSON.stringify(carrito));
 }
